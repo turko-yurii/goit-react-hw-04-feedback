b35a362c1c68fdf6919fc408ba2f19f08d98190f
turko-yurii <turkoburko1993@gmail.com>
2023-10-18 08:42:24 +0300

    hw4feedback

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { Statistics } from './statistic/Statistics';
+import { FeedbackOptions } from './feedbackOptions/FeedbackOptions';
+import { Section } from './section/Section';
+import { Notification } from './notification/Notification';
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  const onLeaveFeedback = event => {
+    const { name } = event.currentTarget;
+
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={onLeaveFeedback}
+      />
+      {countTotalFeedback() ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </Section>
+  );
+};
